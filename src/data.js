c2a98f581468ce1b0767623f0a1f1d3a89540a82
MichaelStarkman <michaelstarkman@gmail.com>
2022-06-14 15:27:42 -0600

    slider

---
+++ src/data.js
@@ -0,0 +1,24 @@
+import shoppingCart from './Components/Images/shopping-cart.png'
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.imgur.com/XQNVTVm.png",
+      title: "Summer Sale",
+      desc: "Massive savings! Shop now for 50% everything listed",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "Popular Sale",
+      desc: "Massive savings! Shop now for 50% everything listed",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "Winter Sale",
+      desc: "Massive savings! Shop now for 50% everything listed",
+      bg: "fbf0f4",
+    },
+  ];
