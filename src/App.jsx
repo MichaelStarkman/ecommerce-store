39de4767bedb0c6ef9dd6b9d7e4d336dda53a8d9
MichaelStarkman <michaelstarkman@gmail.com>
2022-06-15 14:06:36 -0600

    register login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 // import ProductList from "./pages/ProductList";
-
-import Product from "./pages/Product";
-
+// import Product from "./pages/Product";
+// import Register from "./pages/Register";
 // import Home from "./pages/Home";
+
+import Login from "./pages/Login";
+
 const App = () => {
   return (
     <div>
       {/* <Home></Home> */}
       {/* <ProductList></ProductList> */}
-      <Product></Product>
+      {/* <Product></Product> */}
+      {/* <Register></Register> */}
+      <Login></Login>
     </div>
   )
 };
